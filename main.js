c14001e32d345564c9e2c4957a832d061d095074
Niraj Kumar <nirajs9kr@gmail.com>
2024-01-19 21:46:25 +0530

    working

--- main.js
+++ main.js
@@ -28,3 +28,35 @@ navLinks.addEventListener("click", (e) => {
     { once: true }
   );
 });
+
+const explore = document.querySelector(".explore__wrapper-inner");
+
+const exploreContent = Array.from(explore.children);
+
+exploreContent.forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  explore.appendChild(duplicateNode);
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+});
+
+const faq = document.querySelector(".faq__grid");
+
+faq.addEventListener("click", (e) => {
+  const target = e.target;
+  const faqCard = target.closest(".faq__card");
+  if (target.classList.contains("ri-arrow-down-s-line")) {
+    if (faqCard.classList.contains("active")) {
+      faqCard.classList.remove("active");
+    } else {
+      Array.from(faq.children).forEach((item) => {
+        item.classList.remove("active");
+      });
+      faqCard.classList.add("active");
+    }
+  }
+});
